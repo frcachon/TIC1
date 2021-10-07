13dcb2fbb0f2ba551d769d819b5882178cbb8d0d
Francisco Cachon <fracachon@gmail.com>
2021-10-07 12:31:56 -0300

    Se adapta AddEmpleado.fxml y su respectivo controller. Falta mostrar nombres de operadores y realizar el registro. Falta crear tabla en BD, Repository, Entidad, Manager.

--- src/main/java/com/example/demo3/ui/controllers/AddEmpleadoController.java
+++ src/main/java/com/example/demo3/ui/controllers/AddEmpleadoController.java
@@ -5,6 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +16,7 @@ import java.io.IOException;
 @Component
 public class AddEmpleadoController {
 
+
     @FXML
     private AnchorPane emp_pane;
 
@@ -20,11 +24,28 @@ public class AddEmpleadoController {
     private Button atras_button;
 
     @FXML
+    private TextField username_field;
+
+    @FXML
+    private ChoiceBox<String> operador_choiceBox;
+
+    @FXML
+    private Button registrarButton;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
     void goBack(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setControllerFactory(Demo3Application.getContext()::getBean);
         AnchorPane pane = fxmlLoader.load(MainController.class.getResourceAsStream("HomeAdmin.fxml"));
         emp_pane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void registrarEmpleado(ActionEvent event) {
+
     }
 
 }
